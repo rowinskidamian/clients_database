06906faf8b326ee3e1b458e1c2e27ab112439a08
Damian Rowiński <damian.rowinski@gmail.com>
2020-11-16 21:17:35 +0100

    added sql start data and h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into companies(modification_date, company_name, nip) values (NOW(), 'Coca-cola', '92920393');
+insert into companies(modification_date, company_name, nip) values (NOW(), 'Nike', '222323232');
+insert into companies(modification_date, company_name, nip) values (NOW(), 'Netfilx', '11110902');
+
+insert into clients(modification_date, first_name, last_name, company_id) values (NOW(), 'Robert', 'Jankowski', 1);
+insert into clients(modification_date, first_name, last_name, company_id) values (NOW(), 'Andrzej', 'Nowak', 3);
+insert into clients(modification_date, first_name, last_name, company_id) values (NOW(), 'Janina', 'Rodzeń', 2);
