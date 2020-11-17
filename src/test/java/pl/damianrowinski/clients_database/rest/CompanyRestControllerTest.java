bc00812cdfed7d1dc0e1c8d350e0ae2840674805
Damian Rowiński <damian.rowinski@gmail.com>
2020-11-17 14:33:16 +0100

    added dependencies for tests and company rest first test

---
+++ src/test/java/pl/damianrowinski/clients_database/rest/CompanyRestControllerTest.java
@@ -0,0 +1,64 @@
+package pl.damianrowinski.clients_database.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.damianrowinski.clients_database.domain.dto.CompanyDTO;
+import pl.damianrowinski.clients_database.services.CompanyService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = CompanyRestController.class)
+class CompanyRestControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+
+    @MockBean private CompanyService companyService;
+
+    private CompanyDTO companyData1;
+    private CompanyDTO companyData2;
+    private List<CompanyDTO> companyDataList;
+
+    @BeforeEach
+    void init() {
+        companyData1 = new CompanyDTO();
+        companyData1.setId(1);
+        companyData1.setCompanyName("Coca-cola");
+        companyData1.setModificationDate(LocalDateTime.now());
+        companyData1.setNip(111_111);
+
+        companyData2 = new CompanyDTO();
+        companyData2.setId(2);
+        companyData2.setCompanyName("Pepsi Co");
+        companyData2.setModificationDate(LocalDateTime.now().minusDays(2));
+        companyData2.setNip(222_222);
+
+        companyDataList = List.of(companyData1, companyData2);
+    }
+
+
+    @Test
+    void givenMainApiPathShouldReturnListOfCompaniesData() throws Exception {
+        String companyListJSON = objectMapper.writeValueAsString(companyDataList);
+        when(companyService.findAll()).thenReturn(companyDataList);
+
+        mockMvc.perform(get("/api/company"))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(companyListJSON));
+    }
+
+
+}
